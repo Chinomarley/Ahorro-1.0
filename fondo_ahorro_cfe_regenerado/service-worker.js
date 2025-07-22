3bf27dc95d9202e0930e3bfb95ab62fdff195af3
Chinomarley <40522946+Chinomarley@users.noreply.github.com>
2025-07-22 12:01:07 -0600

    Actualizar service-worker.js

--- fondo_ahorro_cfe_regenerado/service-worker.js
+++ fondo_ahorro_cfe_regenerado/service-worker.js
@@ -1,1 +1,19 @@
-// Empty service worker for PWA
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('fondo-ahorro').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.html',
+        '/styles.css',
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
